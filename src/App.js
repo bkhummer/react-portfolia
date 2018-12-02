091ccffe9dc39120147f3e49f38521e513772f8e
Bkhaithem <hboukhrouf@club-freelance.com>
2018-12-02 12:04:48 +0100

    init hot-loader config

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+            <h1>test</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reloa.
           </p>
           <a
             className="App-link"
